62dfbcec8bd502a83c997a2ce3f2315dfc03e0f7
Chris <Christopherjorge47@icloud.com>
2024-05-14 09:48:19 -0400

    Functionality to recieve temperature of each day from user and calculated the average and days above average added

--- main.py
+++ main.py
@@ -9,9 +9,32 @@ while True:
     if numDays.isdigit():
         confirm = input(f'You entered {numDays}, is this correct y / n? ').upper()
         if confirm == 'Y':
+            numDays = int(numDays)
             False
             break
         else:
             continue
     else:
         print('Invalid, please enter a number')
+
+day = 1
+while day <= numDays:
+    temperature = input(f'Enter the temperature for day {day}: ')
+    if temperature.isdigit():
+        temperatureArray.append(int(temperature))
+        day += 1
+    else:
+        continue
+
+average = 0
+daysAbove = 0
+for temp in temperatureArray:
+    average += temp
+
+average = round(average / numDays, 2)
+
+for temp in temperatureArray:
+    if temp > average:
+        daysAbove += 1
+
+print(f'Average Temperature: {average} \nDays above average: {daysAbove}')
